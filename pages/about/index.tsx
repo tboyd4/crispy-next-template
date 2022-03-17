6fc4e5b45409ae083dc759547a6f245472ca09a4
Tyler Boyd <ty@tyboyd.dev>
2022-03-17 11:59:20 -0500

    testing a page add

---
+++ pages/about/index.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="bg-slate-400 text-white p-4 rounded w-1/2 mx-auto my-12">
+      hello from react, but the about page
+    </div>
+  )
+}
