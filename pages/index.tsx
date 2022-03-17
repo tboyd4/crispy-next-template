167b427ba40dd3bdf7cb91165a2dc68e19149d32
Tyler Boyd <ty@tyboyd.dev>
2022-03-17 11:55:04 -0500

    initial commit and setup

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-slate-400 text-white p-4 rounded w-1/2 mx-auto my-12">
+      hello from react
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./components/**/*.tsx', './pages/**/*.tsx','./public/**/*.html'], //add this line
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
